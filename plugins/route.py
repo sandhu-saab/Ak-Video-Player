064ff2e5275b7c4cf8245680ca908a7c274405ca
Mr AK <abcdwalaokk@gmail.com>
2025-06-26 18:40:02 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -25,7 +25,7 @@ html_content = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to VJ Disk</title>
+    <title>Welcome to Ak Disk</title>
     <style>
         body {
             margin: 0;
