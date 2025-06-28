683a46f373f5eb687b8aa82a10085d4d506db6c3
sandhu-saab <sandhusaab61291@gmail.com>
2025-06-28 06:58:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @VJ_Botz 
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+
 # Ask Doubt on telegram @KingVJ01
 
 import sys, glob, importlib, logging, logging.config, pytz, asyncio
@@ -31,16 +32,19 @@ from TechVJ.bot.clients import initialize_clients
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
+
 TechVJBot.start()
 TechVJBackUpBot.start()
-loop = asyncio.get_event_loop()
 
+loop = asyncio.get_event_loop()
 
 async def start():
     print('\n')
     print('Initalizing Your Bot')
+
     bot_info = await TechVJBot.get_me()
     await initialize_clients()
+
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -52,24 +56,29 @@ async def start():
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
             print("Tech VJ Imported => " + plugin_name)
+
     if ON_HEROKU:
-        asyncio.create_task(ping_server())
+        asyncio.create_task(ping_server())  # Heroku keep-alive ping
+
     me = await TechVJBot.get_me()
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
+
     await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+
+    # Start aiohttp web server with alive route
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    await idle()
+    port = int(PORT) if PORT else 8080
+    await web.TCPSite(app, bind_address, port).start()
 
+    await idle()
 
 if __name__ == '__main__':
     try:
         loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
-
