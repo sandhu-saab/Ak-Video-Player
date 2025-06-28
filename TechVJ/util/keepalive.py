1323417bdecbf47ea0da17f34082772772a0bd86
sandhu-saab <sandhusaab61291@gmail.com>
2025-06-28 07:02:27 +0530

    Update keepalive.py

--- TechVJ/util/keepalive.py
+++ TechVJ/util/keepalive.py
@@ -4,18 +4,17 @@ import aiohttp
 import traceback
 from info import *
 
-
 async def ping_server():
-    sleep_time = PING_INTERVAL
+    sleep_time = int(PING_INTERVAL) if PING_INTERVAL else 300  # Default to 5 minutes
+
     while True:
         await asyncio.sleep(sleep_time)
         try:
-            async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(total=10)
-            ) as session:
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
                 async with session.get(STREAM_URL) as resp:
                     logging.info("Pinged server with response: {}".format(resp.status))
-        except TimeoutError:
-            logging.warning("Couldn't connect to the site URL..!")
-        except Exception:
+        except aiohttp.ClientError as e:
+            logging.warning(f"Couldn't connect to the site URL..! Reason: {e}")
+        except Exception as e:
+            logging.error("Unexpected error during ping:")
             traceback.print_exc()
